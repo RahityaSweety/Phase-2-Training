ac382050bb3bcfae6e764e7c5404a2e5452d7c90
RahityaSweety <164530105+RahityaSweety@users.noreply.github.com>
2024-05-31 12:54:42 +0530

    Add files via upload

---
+++ Adjacency matrix.py
@@ -0,0 +1,14 @@
+#Adjacency Matrix
+n, m = map(int, input().split())
+matrix = []
+for i in range(n):
+   row = [0] * n 
+    #[0, 0, 0, 0, 0]
+   matrix.append(row)
+print(matrix) 
+
+for i in range(m):
+    u, v = map(int, input().split())
+    matrix[u][v] = 1 
+    matrix[v][u] = 1  
+print(matrix)
